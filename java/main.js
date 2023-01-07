f5200fd214b488e7b71ed58c65551e18941e463f
Facundo <facuvallejo00@gmail.com>
2023-01-07 01:38:59 -0300

    se agrega proyecto

---
+++ java/main.js
@@ -0,0 +1,20 @@
+let precio = Number(prompt("¿Cual es el precio en dolares?"))
+
+if (precio == ""){
+    alert("por favor, ingresa un precio en dolares")
+    while (precio == "") {
+        alert("por favor, ingresa un precio en dolares")
+    }  
+}
+else (precio == Number); {
+    function sumar(precio,){
+    resultado = Number(precio)
+    }}
+
+    function mostrar(mensaje){
+        console.log(mensaje)
+    }
+
+    sumar (Number(precio+10))
+    mostrar(resultado);
+    alert(resultado)
