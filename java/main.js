383ef71ddd0fc76d98399207e20c4a9ccbe5b3c6
Facundo <facuvallejo00@gmail.com>
2023-01-19 16:53:33 -0300

    se agrega diferentes monedas

--- java/main.js
+++ java/main.js
@@ -21,3 +21,16 @@ let resultado = precio * 325.74
 mostrar(resultado);
 
 alert(`El total es de $ ${resultado}`)
+
+
+function Monedas (nombre, valor, subio){
+ this.nombre = nombre;
+ this.valor = valor;
+ this.cambio = subio;
+};
+var moneda1 = new Monedas("Dolar", "Su valor es de $370",false)
+var moneda2 = new Monedas("Euro","su valor es de $198",true)
+var moneda3 = new Monedas("Real","su valor es de $35",true)
+
+var monedas = [moneda1, moneda2, moneda3]
+console.log(monedas)
